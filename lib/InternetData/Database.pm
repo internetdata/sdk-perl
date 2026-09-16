0bf4452d7c2fc9b5a4cdce2af8822704840503d5
Uman Shahzad <uman@mslm.io>
2026-09-16 11:57:10 +0200

    Bump to 1.4.0

--- lib/InternetData/Database.pm
+++ lib/InternetData/Database.pm
@@ -7,7 +7,7 @@ use Carp ();
 
 use InternetData::Error;
 
-our $VERSION = '1.3.0';
+our $VERSION = '1.4.0';
 
 # The formats a database is published in. Anything else is refused before it
 # reaches the API, whose 400 would cost a round trip and name nothing to act on.
